a7114f0819d82f8ee1e96d0edcc3017336a26d19
obaydullaa <cr7obaydul@gmail.com>
2022-12-14 00:37:26 +0600

    Add first class php Data type.

---
+++ Day-01-Data-Type-( 14-12-22)/dataType.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+* Data Type in PHP: 8
+================================
+1. String: It is string thats means it will be quotation single or double quote. Exam: 'obaydul', 'Bangladesh'.
+2. Integer: Its is a number. It's not need for quote. Exam: 1,2,3,4,5
+3. Float/ Double: A float is a number with a decimal point and can be extended to exponential form. Exam: 1.12, 2.12
+4. Boolean: True of False.
+5. Array: Collection of multiple data. Exam: array['obaydulla', 'Bangladesh']; OR ['Obaydulla', 'Bangladesh'];
+6. Null: Empty.
+7. Object.
+8. Resource.
+
+*/
