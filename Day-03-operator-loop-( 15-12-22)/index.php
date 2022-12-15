d81c65544398c3fb10b4a8a8c50963a86a297d54
obaydullaa <cr7obaydul@gmail.com>
2022-12-15 09:31:37 +0600

    added Day-3 and quotetion use and scipe in php.

---
+++ Day-03-operator-loop-( 15-12-22)/index.php
@@ -0,0 +1,20 @@
+<!-- PHP Part-18: string data with variables  -->
+<?php 
+$name = 'obaydul';
+$age = 20;
+$job = 'Frontend Developer';
+
+// write variable in single quote 
+// echo 'my name is ' .$name.' and I am a ' .$job. ' And I am ' .$age.' years old ';
+
+echo "\n";
+// write variable in dubble quote 
+
+// waye one 
+// echo " my name is " .$name." and I am a " .$job. " And I am " .$age." years old ";
+
+// waye-2 
+// echo " my name is $name and I am a $job And I am $age years old ";
+
+
+echo 'I\'m Obaydul';
---
+++ PHP-all-note.php
@@ -0,0 +1,15 @@
+<!-- class PHP-18  -->
+
+// write variable in single quote 
+
+echo 'my name is ' .$name.' and I am a ' .$job. ' And I am ' .$age.' years old ';
+
+// write variable in dubble quote 
+
+// waye one 
+echo " my name is " .$name." and I am a " .$job. " And I am " .$age." years old ";
+
+// waye-2 
+echo " my name is $name and I am a $job And I am $age years old ";
+
+<!-- echo 'I\'m Obaydul'; -->
