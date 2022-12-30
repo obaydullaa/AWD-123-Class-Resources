4a76ff0fb48cd053097f41da9bdf5360ef6f1c07
obaydullaa <cr7obaydul@gmail.com>
2022-12-30 23:18:31 +0600

    File project create.

---
+++ Day-08-from-project(28-12-22)/From-Project/file-upload.php
@@ -0,0 +1,37 @@
+
+<!-- 
+  PHP part:  67 ( Data send by a form  ) // From design
+  PHP part:  68 ( form setup )
+  PHP part:  69 ( form Validation )
+  PHP part:  70 ( form Validation Required)
+  PHP part:  71( form Validation email)
+  PHP part:  72(Project Upload in github)
+  PHP part:  73(email check)
+     end() => for get array last child.
+  ** filter_var($email, FILTER_VALIDATE_EMAIL) = for email validation.
+
+  PHP part:  74(Project Upload in github)
+  PHP part 75 ( cell validation )
+
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Design</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+    
+   
+
+    <script src="assets/js/jquery-3.6.3.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+
+ 
