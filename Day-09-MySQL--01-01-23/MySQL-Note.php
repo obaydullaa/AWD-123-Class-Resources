e052918bdde59fdc72c97b8e3951eee1f7b7a1a7
obaydullaa <cr7obaydul@gmail.com>
2023-01-02 00:23:50 +0600

    PHP MySql Admin panel Intro.

--- Day-09-MySQL--01-01-23/MySQL-Note.php
+++ Day-09-MySQL--01-01-23/MySQL-Note.php
@@ -1,6 +1,8 @@
 /**
 * MySQL part 01 ( basic )
 *=============================
+* MySQL and PHPAdmin panel Intro
+* 
 * SQL => Structured Query Language.
 * MySQL is 2 types: See Queel & No Seel Queel
 *
