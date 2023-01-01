29b4fd94d7abcc7d9bfcd5fb448f6e1194d4102c
obaydullaa <cr7obaydul@gmail.com>
2023-01-02 00:43:50 +0600

    Table create and database intro.

--- Day-09-MySQL--01-01-23/MySQL-Note.php
+++ Day-09-MySQL--01-01-23/MySQL-Note.php
@@ -2,7 +2,7 @@
 * MySQL part 01 ( basic )
 *=============================
 * MySQL and PHPAdmin panel Intro
-* 
+* Done => 1---5
 * SQL => Structured Query Language.
 * MySQL is 2 types: See Queel & No Seel Queel
 *
