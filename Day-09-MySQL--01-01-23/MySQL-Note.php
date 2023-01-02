cb819f27d0d66112894bdbbc94a8a90e978cf3b6
obaydullaa <cr7obaydul@gmail.com>
2023-01-02 09:10:51 +0600

    Database create, update and delete in manualy in phpmyadmin.

--- Day-09-MySQL--01-01-23/MySQL-Note.php
+++ Day-09-MySQL--01-01-23/MySQL-Note.php
@@ -2,7 +2,7 @@
 * MySQL part 01 ( basic )
 *=============================
 * MySQL and PHPAdmin panel Intro
-* Done => 1---5
+* Done => 1---6
 * SQL => Structured Query Language.
 * MySQL is 2 types: See Queel & No Seel Queel
 *
