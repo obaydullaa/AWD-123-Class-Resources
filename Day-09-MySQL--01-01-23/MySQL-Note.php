d43e8d38d4d9145b1b416fc6011b9839ecb73d26
obaydullaa <cr7obaydul@gmail.com>
2023-01-03 00:37:12 +0600

    Mysqul database user name and passwor.

--- Day-09-MySQL--01-01-23/MySQL-Note.php
+++ Day-09-MySQL--01-01-23/MySQL-Note.php
@@ -3,6 +3,7 @@
 *=============================
 * MySQL and PHPAdmin panel Intro
 * Done => 1---6
+* MySQL part 07 ( db username ) create in PHP Myadmin.
 * SQL => Structured Query Language.
 * MySQL is 2 types: See Queel & No Seel Queel
 *
