6421880331172c9737554a6ba3317ee4316c04ba
obaydullaa <cr7obaydul@gmail.com>
2023-01-04 01:56:44 +0600

    Connect DB.

---
+++ Day-09-MySQL--01-01-23/DB/index.php
@@ -0,0 +1,17 @@
+<?php
+
+/***
+*
+* MySQL part 08 (  db connection  )
+*/
+
+// Server Connect
+// mysql_connect(); // Old 
+// mysql_select_db(); // Old
+
+// mysqli_connect(); // new Not use
+// new mysqli(); // new use it bcz it's oop
+// new PDO(); // new use it bcz it's oop
+
+
+new mysqli('localhost','obaydul', 123456);
--- MySQL-Note.php
+++ MySQL-Note.php
@@ -6,9 +6,12 @@
 * MySQL part 07 ( db username ) create in PHP Myadmin.
 * SQL => Structured Query Language.
 * MySQL is 2 types: See Queel & No Seel Queel
-*
-*
-*
-*
-*
- */
+* 
+*/
+/* 
+* MySQL part 08 (  db connection  )
+*/
+<?php
+$network = new mysqli('localhost', 'obaydul', '123456');
+
+$network -> prepare(); // Or query() statement
