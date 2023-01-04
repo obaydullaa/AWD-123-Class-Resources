93cae9cbe409b9a787afa167de2c1c88a9e2c006
obaydullaa <cr7obaydul@gmail.com>
2023-01-05 01:32:14 +0600

    Table create by php.

--- Day-09-MySQL--01-01-23/DB/index.php
+++ Day-09-MySQL--01-01-23/DB/index.php
@@ -13,7 +13,28 @@
 // new mysqli(); // new use it bcz it's oop
 // new PDO(); // new use it bcz it's oop
 
+/**
+ * MySQL part 09 ( CREATE and drop )
+ */
 
-$network = new mysqli('localhost','obaydul', 123456);
+// $network = new mysqli('localhost','obaydul', 123456); // connect database
 // $network -> query("CREATE DATABASE sm"); // DB create
-$network -> query("DROP DATABASE sm"); // DB create
+// $network -> query("DROP DATABASE sm"); // DB Drop
+
+
+/**
+ * MySQL part 10 (  CREATE TABLE  )
+ */
+
+ $network = new mysqli('localhost','obaydul', 123456, 'sms'); // connect database
+ 
+ $network -> query("CREATE TABLE users (
+    id int(11),
+    name varchar(100),
+    age int(3),
+    cell varchar(20),
+    )"); 
+
+
+
+ 
