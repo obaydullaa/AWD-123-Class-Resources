bd2450b58a00a08087c252f1ef3fdc545c5a9125
obaydullaa <cr7obaydul@gmail.com>
2023-01-21 09:52:40 +0600

    Create student table in database.

---
+++ Student-Crud.php
@@ -0,0 +1,27 @@
+// Note project
+1. Template setup
+2. Connection function
+
+
+/**
+* Student Crudv part 01 & 2 ( Template setup & Connect function)
+*/
+
+Folder: app-> db.php * functions.php 
+db.php:- 
+
+    /**
+    * Make a Database connection
+    */
+
+    $host = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $db = 'stc';
+
+    function connect() {
+        global $host, $user, $pass, $db; // global variable
+        return $connection = new mysqli($host, $user, $pass, $db);
+    }
+
+    
