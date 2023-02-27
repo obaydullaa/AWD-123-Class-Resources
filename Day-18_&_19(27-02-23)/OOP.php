6608d907c1ca6dcf9678f46bd9ca7d786d566d10
obaydullaa <cr7obaydul@gmail.com>
2023-02-27 09:23:04 +0600

    static property and method access.

--- Day-18_&_19(27-02-23)/OOP.php
+++ Day-18_&_19(27-02-23)/OOP.php
@@ -1,27 +1,56 @@
+<?php
 /** 
-   // Object Oriented Programming - PHP 
-<!--     	
-        - What is OOP & Why 
-        - class & Object 
-        - property & Method 
-        - Use property & Method out of a class 
-        - Use property & Method in of a class 
-        - static method & property & its use  -->
-
-
+ *  Object Oriented Programming - PHP 
+ * 
+ * - What is OOP & Why 
+ * - class & Object 
+ * - property & Method 
+ * - Use property & Method out of a class
+ * - Use property & Method in of a class
+ * - static method & property & its use
+ * 
 */
 
-<?php
+
 
 //class & Object 
 
 // class
-class Student {
-    public $name = "Obaydul \n"; // Property
+// class Student {
+//     public $name = "Obaydul \n"; // Property
 
 
-    public function dev() {
-        echo "My Name is $this->name"; // Method
+//     public function dev() {
+//         echo "My Name is $this->name"; // Method
+//     }
+
+//     public function info() {
+//         echo $this->dev();
+//     }
+
+//     public static function tomi() {
+//         echo "I am Tomi.";
+//     }
+
+// }
+// Object
+// $stu = new Student;
+// echo $stu->name;
+// $stu->info();
+
+// Student::tomi();
+
+/**
+ * static method & property & its use 
+ * ====================================================
+ */
+
+ class Student {
+    public static $name = "Obaydul \n"; // Property
+
+
+    public static function dev() {
+        echo "My Name is Obaydulla \n"; // Method
     }
 
     public function info() {
@@ -33,9 +62,6 @@ class Student {
     }
 
 }
-// Object
-// $stu = new Student;
-// echo $stu->name;
-// $stu->info();
-
-Student::tomi();
+// Access static property and method.
+Student::dev();
+echo Student::$name;
