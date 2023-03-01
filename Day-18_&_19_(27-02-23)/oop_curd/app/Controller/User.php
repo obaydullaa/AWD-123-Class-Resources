ee57785cbf5141b4c8580b29610345af736d2729
obaydullaa <cr7obaydul@gmail.com>
2023-03-01 07:44:16 +0600

    create database design and some function create.

--- Day-18_&_19_(27-02-23)/oop_curd/app/Controller/User.php
+++ Day-18_&_19_(27-02-23)/oop_curd/app/Controller/User.php
@@ -5,6 +5,73 @@
  */
 
  class User {
+    
+    private $host = 'localhost';
+    private $user = 'root';
+    private $pas = '';
+    private $db = 'oop129';
+    private $connection;
+
+
+    /**
+     * Database Connection
+     */
+
+     public function connection() {
+        return $this->connection = new mysqli($this->host,$this->user,$this->pas, $this->db);
+     }
+
+    /**
+    * Create Data
+    */
+    protected function create () {
+
+    }
+
+    /**
+    * find Data
+    */
+    protected function find () {
+
+    }
+
+    /**
+    * delete Data
+    */
+    protected function delete () {
+
+    }
+    /**
+    * update Data
+    */
+    protected function update () {
+
+    }
+
+    /**
+    * all Data
+    */
+    protected function all () {
+
+    }
+
+    /**
+    * where Data
+    */
+    protected function where () {
+
+    }
+
+    /**
+    * onWhere Data
+    */
+    protected function onWhere () {
+
+    }
+    
+
+
+
 
     
  }
--- Day-18_&_19_(27-02-23)/oop_curd/app/Support/Database.php
+++ Day-18_&_19_(27-02-23)/oop_curd/app/Support/Database.php
@@ -5,6 +5,6 @@
  */
 
  class Database {
-
+	
     
  }
