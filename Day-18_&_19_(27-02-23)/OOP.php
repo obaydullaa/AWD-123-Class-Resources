1717ac2daae907d485872327bac593ce5a655a07
obaydullaa <cr7obaydul@gmail.com>
2023-03-03 17:43:30 +0600

    method chain and type hinting.

--- Day-18_&_19_(27-02-23)/OOP.php
+++ Day-18_&_19_(27-02-23)/OOP.php
@@ -760,3 +760,51 @@ index.php  ->
     $c = new C;
     echo $c ->dev();
     
+    /**
+     * OOP part 29 ( method chain )
+     * =================================================
+     * 
+     */
+
+     class Student {
+        private  $name = 'Obaydul';
+        private  $age = 30;
+        private  $skill = "js Developer";
+    
+        /**
+         * Set Student Name
+         */
+        public function setName(string $name) { // Type hinting
+            $this->name = $name;
+            return $this;
+        }
+    
+        /**
+         * Set Age chain
+         */
+    
+         public function setAge($age) {
+            $this->age = $age;
+            return $this;
+         }
+    
+        /**
+         * Set skill chain
+         */
+         public function setSkill($skill) {
+            $this->skill = $skill;
+            return $this;
+         }
+    
+        /**
+         * Info Methods
+         * 
+         */
+        public function info() {
+            return "My Name is {$this->name} & I am {$this->age} year old and I am {$this->skill}."; // chain method
+        }
+    }
+    
+    $stu = new Student;
+    
+    echo $stu -> setSkill('PHP Developer') -> setAge(20)-> setName('Rifat')->info();
--- Day-18_&_19_(27-02-23)/interface-implements.php
+++ Day-18_&_19_(27-02-23)/interface-implements.php
@@ -1,20 +1,44 @@
 <?php
 
-Trait A {
-    public function dev() {
-        return "I am from dev A";
+class Student {
+    private  $name = 'Obaydul';
+    private  $age = 30;
+    private  $skill = "js Developer";
+
+    /**
+     * Set Student Name
+     */
+    public function setName($name) {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Set Age chain
+     */
+
+     public function setAge($age) {
+        $this->age = $age;
+        return $this;
+     }
+
+    /**
+     * Set skill chain
+     */
+     public function setSkill($skill) {
+        $this->skill = $skill;
+        return $this;
+     }
+
+    /**
+     * Info Methods
+     * 
+     */
+    public function info() {
+        return "My Name is {$this->name} & I am {$this->age} year old and I am {$this->skill}.";
     }
 }
 
-class B {
-    public function food() {
-        return "I am from B";
-    }
-}
-class C extends B {
-    use A;
-}
+$stu = new Student;
 
-$c = new C;
-
-echo $c ->dev();
+echo $stu -> setSkill('PHP Developer') -> setAge(20)-> setName('Rifat')->info();
