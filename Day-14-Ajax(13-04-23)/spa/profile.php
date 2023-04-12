14ce59d5a651827c9b87fb4738558807ae807a9d
obaydullaa <cr7obaydul@gmail.com>
2023-04-13 05:04:12 +0600

    Design ajax template.

---
+++ Day-14-Ajax(13-04-23)/spa/profile.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Development Area</title>
+	<!-- ALL CSS FILES  -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+<body>
+	
+	
+
+	<div class="wrap-table shadow">
+		<div class="card">
+			<div class="card-body">
+				<h2>All Data</h2>
+				<img style="width: 200px; height: 200px; border-radius: 50%; margin: 50px auto;" src="assets/media/img/pp_photo/_MG_1216.jpg" alt="">
+				<h2>Md. Obaydulla</h2>
+			</div>
+		</div>
+	</div>
+	
+
+
+
+
+
+
+
+	<!-- JS FILES  -->
+	<script src="assets/js/jquery-3.4.1.min.js"></script>
+	<script src="assets/js/popper.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/custom.js"></script>
+</body>
+</html>
