dc890cd89e082b506a9f374243eafe76f6be5425
obaydullaa <cr7obaydul@gmail.com>
2023-04-23 10:25:40 +0600

    Added note file in ajax code.

--- Day-14-Ajax(13-04-23)/spa/Ajax-Note.php
+++ Day-14-Ajax(13-04-23)/spa/Ajax-Note.php
@@ -24,8 +24,36 @@
         let cell = $('#cell').val();
         let username = $('#username').val();
 
-        alert(name + email + cell + username);
-        return false;
+        $.ajax({
+            url: 'ajax_template/ajax.php',
+            method: "POST",
+            data: {
+                name : name,
+                email: email,
+                cell: cell,
+                username: username
+            },
+            success: function(data) {
+                alert('Student added successful ');
+                // data reset
+                $('#name').val('');
+                $('#email').val('');
+                $('#cell').val('');
+                $('#username').val('');
+            }
+        })
+
+        return false; 
     });
+    
+    // Get value and pus in mysql server
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $cell = $_POST['cell'];
+    $username= $_POST['username'];
+
+    $connection = new mysqli('localhost', 'root', '', 'ajax');
+    $connection->query("INSERT INTO students (name, email, cell, username) VALUES ('$name', '$email', '$cell', '$username')");
+
 
 </script>
