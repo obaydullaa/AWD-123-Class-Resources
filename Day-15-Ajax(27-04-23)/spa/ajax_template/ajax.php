8bb9848243ddc97ebf88efedb4d3307dce357b04
obaydullaa <cr7obaydul@gmail.com>
2023-04-28 02:16:35 +0600

    Added file use ajax.

--- Day-15-Ajax(27-04-23)/spa/ajax_template/ajax.php
+++ Day-15-Ajax(27-04-23)/spa/ajax_template/ajax.php
@@ -6,6 +6,10 @@ $email = $_POST['email'];
 $cell = $_POST['cell'];
 $username= $_POST['username'];
 
-$connection = new mysqli('localhost', 'root', '', 'ajax');
-$connection->query("INSERT INTO students (name, email, cell, username) VALUES ('$name', '$email', '$cell', '$username')");
+$file_name = time() . $_FILES['photo']['name'];
+$file_tmp_name = $_FILES['photo']['tmp_name'];
 
+$connection = new mysqli('localhost', 'root', '', 'ajax');
+$connection->query("INSERT INTO students (name, email, cell, username, photo) VALUES ('$name', '$email', '$cell', '$username', '$file_name')");
+
+move_uploaded_file($file_tmp_name, '../photos/' . $file_name);
--- Day-15-Ajax(27-04-23)/spa/create.php
+++ Day-15-Ajax(27-04-23)/spa/create.php
@@ -4,7 +4,7 @@
 		<div class="card-body">
 			<h2>Add new Student</h2>
 			<div class="msg"></div>
-			<form id="student_form" method="POST">
+			<form id="student_form" method="POST" enctype="multipart/form-data">
 				<div class="form-group">
 					<label for="">Name</label>
 					<input name="name" class="form-control" type="text">
@@ -20,6 +20,10 @@
 				<div class="form-group">
 					<label for="">Username</label>
 					<input name="username" class="form-control" type="text">
+				</div>
+				<div class="form-group">
+					<label for="">Photo</label>
+					<input name="photo" class="form-control" type="file">
 				</div>
 				<div class="form-group">
 					<input class="btn btn-primary" type="submit" value="Sign Up">
