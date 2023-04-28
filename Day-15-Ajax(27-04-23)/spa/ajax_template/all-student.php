83ae4aab34291773902a781885dba4061eb1dca2
obaydullaa <cr7obaydul@gmail.com>
2023-04-28 09:25:45 +0600

    show image in html by ajax.

--- Day-15-Ajax(27-04-23)/spa/ajax_template/all-student.php
+++ Day-15-Ajax(27-04-23)/spa/ajax_template/all-student.php
@@ -13,7 +13,7 @@ while($stu = $data->fetch_object()) :
         <td><?php echo $stu->email  ?></td>
         <td><?php echo $stu->cell  ?></td>
         <td><?php echo $stu->username  ?></td>
-        <td><img src="assets/media/img/pp_photo/istockphoto-615279718-612x612.jpg" alt=""></td>
+        <td><img src="photos/<?php echo $stu->photo; ?>" alt=""></td>
         <td>
             <a class="btn btn-sm btn-info" href="#">View</a>
             <a class="btn btn-sm btn-warning" href="#">Edit</a>
