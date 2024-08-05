c53ce0703a00ad092674a3b8ab8df5c531357a1d
obaydullaa <cr7obaydul@gmail.com>
2024-08-05 06:56:14 +0600

    oop php muli extend

--- php-oop.php
+++ php-oop.php
@@ -1,6 +1,8 @@
 <?php
 
-
+/**
+ * part: 01 Class and Object
+ */
 
 // class Student  {
 //     public $name = "student"; // Class ar modde variable nile setake propery bole empne class ar modde methods nile seta ke mehods bole.
@@ -39,20 +41,150 @@
 // echo $profile->profileName(); // echo  => My name is Obaydullla and email obaydullla@gmail.com
 
 
+/**
+ * 06: Static Properties and mehtods 
+ */
 
-class Dev {
-    public static $age = 50;
-    public static $name = "Obaydulla";
+// class Dev {
+//     public static $age = 50;
+//     public static $name = "Obaydulla";
 
-    public static function name () {
-        return "My name is " . Dev::$name;
-    }
+//     public static function name () {
+//         return "My name is " . Dev::$name;
+//     }
 
-}
-echo Dev::name();
+// }
+// echo Dev::name();
 
 // $out = New Dev;
 
 // echo Dev::$age;
 // echo "<br>";
 // echo $out->age;
+
+
+/**
+ * Part 07 Const 
+ */
+
+ /**
+ * 06: Static Properties and mehtods 
+ */
+
+// class Dev {
+//     const AGE = 50;
+//     const NAME = "Obaydulla";
+//     public static $name = "Obaydulla";
+
+//     public static function name () {
+//         return "My name is " . Dev::NAME;
+//     }
+
+// }
+// echo Dev::name();
+
+
+/**
+ *  part 08 ( construct, dectrudct ) Mazic or building methods;
+ */
+
+/**
+ * construct, dectrudct  holo construct sobar upore call hobe hobe r  dectrudct  sobar niche call hobe maje baki golo method ba property thak be. 
+ * construct and dectrudct ar khetre object ar instance create korle auto call hobe;
+ * 
+ *  */
+
+// class Staff {
+//     public  $name = "Obaydulla";
+
+//     public function work() {
+//         return "We are laravel Developer";
+//     }
+
+//     public function __construct() {
+//         echo "<hr> I am construct <hr>";
+//     }
+
+//     public function __destruct() {
+//         echo "<hr> I am Destruct <hr>";
+//     }
+
+
+// }
+
+// $staff = new Staff();
+
+
+// echo $staff->work();
+// echo "<br>";
+// echo $staff->name;
+
+
+/**
+ * OOP part 10 ( extends or inheritance ) 
+ */
+
+ 
+// class A {
+//     public function dev() {
+//         echo "I am Python Developer";
+//     }
+//     public function food() {
+//         echo "I am Love Food";
+//     }
+// }
+
+
+// class B extends A {
+//     public function learn() {
+//         echo "I Love learn Laravel ";
+//     }
+// }
+
+
+// $b = new B();
+
+// echo $b->dev();
+
+
+
+/**
+ * OOP part 11 ( multi extends )
+ */
+
+ 
+class A {
+    public function dev() {
+        echo "I am Python Developer";
+    }
+    public function food() {
+        echo "I am Love Food";
+    }
+}
+
+
+class B extends A {
+    public function learn() {
+        echo "I Love learn Laravel ";
+    }
+}
+
+class C extends B {
+    public function read() {
+        // echo "I am mehtod of C"; 
+
+        // echo  $this->dev();
+        echo  parent::learn();
+    }
+}
+
+$b = new  C();
+
+echo $b->read();
+
+
+
+
+
+
+
